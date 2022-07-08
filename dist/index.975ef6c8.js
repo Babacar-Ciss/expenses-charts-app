69a1dccaae80740756a5b0a87544fe53373be932
Babacar Ciss <kratosnvidia@gmail.com>
2022-07-08 18:54:28 +0000

    adding js

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -536,14 +536,19 @@ function hmrAcceptRun(bundle, id) {
 var _dataJson = require("../data.json");
 //Variables
 let currentIndex;
-let previousIndex;
+let arrayExpense = [];
+let totalExpense;
+let currentBalance = 921.48;
 // Selecteurs //
 const charts = document.querySelectorAll(".container__spending-card-graphics-days");
+const totalExpenseDOM = document.querySelector("#expenses");
+const balanceDOM = document.querySelector("#balance");
 //Récuperation du jour sous format 3 lettres(thu)
-let date = new Date();
-let currentDate = date.toDateString().slice(0, 3).toLowerCase();
+let date = new Date().toLocaleDateString("en-us", {
+    weekday: "short"
+}).toLocaleLowerCase();
 //Récupération de l'index du jour
-currentIndex = _dataJson.findIndex((data)=>data.day === currentDate);
+currentIndex = _dataJson.findIndex((data)=>data.day === date);
 // Activation jour courant
 charts[currentIndex].childNodes[3].style.backgroundColor = "#76B5BC";
 charts[currentIndex].insertAdjacentHTML("beforeend", `<p> $${_dataJson[currentIndex].amount} </p>`);
@@ -553,19 +558,34 @@ charts.forEach((chart, index)=>{
     chart.addEventListener("mouseover", ()=>{
         cleanPreviousDisplay(charts);
         if (currentIndex === index) return;
-        else chart.insertAdjacentHTML("beforeend", `<p> $${_dataJson[index].amount} </p>`);
+        else {
+            chart.insertAdjacentHTML("beforeend", `<p> $${_dataJson[index].amount} </p>`);
+            chart.lastChild.setAttribute("class", "styleChart");
+        }
     });
-    // mouseout  pour enlever l'affichage le montant dépensé
+    // mouseout pour enlever l'affichage le montant dépensé
     chart.addEventListener("mouseout", ()=>{
         cleanPreviousDisplay(charts);
+        chart.classList.remove("styleChart");
     });
 });
+// Fonction pour désactiver tous les états précédents
 function cleanPreviousDisplay(els) {
     els.forEach((el)=>{
         if (el.children.length === 3) el.removeChild(el.lastChild);
     });
     charts[currentIndex].insertAdjacentHTML("beforeend", `<p> $${_dataJson[currentIndex].amount} </p>`);
 }
+// Conversion obj en array of expenses
+_dataJson.forEach((data)=>arrayExpense.push(data.amount));
+// Calcul dépense total semaine
+totalExpense = arrayExpense.reduce((prev, curr)=>{
+    return prev + curr;
+}, 0);
+// mise a jour du DOM total dépense
+totalExpenseDOM.textContent = "$" + totalExpense.toFixed(2);
+// mise a jour DOM balance
+balanceDOM.textContent = "$" + (currentBalance - totalExpense).toFixed(2);
 
 },{"../data.json":"cn6Iz"}],"cn6Iz":[function(require,module,exports) {
 module.exports = JSON.parse('[{"day":"mon","amount":17.45},{"day":"tue","amount":34.91},{"day":"wed","amount":52.36},{"day":"thu","amount":31.07},{"day":"fri","amount":23.39},{"day":"sat","amount":43.28},{"day":"sun","amount":25.48}]');
--- src/index.js
+++ src/index.js
@@ -3,22 +3,25 @@ import * as datas from "../data.json" ;
 
 //Variables
 let currentIndex;
-let previousIndex;
+let arrayExpense = [];
+let totalExpense;
+let currentBalance = 921.48;
+
 
 // Selecteurs //
-const charts = document.querySelectorAll(".container__spending-card-graphics-days")
+const charts = document.querySelectorAll(".container__spending-card-graphics-days");
+const totalExpenseDOM = document.querySelector("#expenses");
+const balanceDOM = document.querySelector("#balance");
 
 //Récuperation du jour sous format 3 lettres(thu)
-let date = new Date();
-let currentDate = date.toDateString().slice(0,3).toLowerCase();
+let date = new Date().toLocaleDateString('en-us', { weekday:"short"}).toLocaleLowerCase();
 
 //Récupération de l'index du jour
-currentIndex = datas.findIndex(data => data.day === currentDate);
+currentIndex = datas.findIndex(data => data.day === date);
 
 // Activation jour courant
 charts[currentIndex].childNodes[3].style.backgroundColor = '#76B5BC';
 charts[currentIndex].insertAdjacentHTML("beforeend", `<p> $${datas[currentIndex].amount} </p>`);
-
 
 
 // Survol (Focus) pour récupérer le montant dépensé
@@ -28,17 +31,20 @@ charts.forEach((chart,index) => {
         chart.addEventListener("mouseover", () => {
             cleanPreviousDisplay(charts);
             if(currentIndex === index) return
-            else chart.insertAdjacentHTML("beforeend", `<p> $${datas[index].amount} </p>`);               
+            else {
+                chart.insertAdjacentHTML("beforeend", `<p> $${datas[index].amount} </p>`);
+                chart.lastChild.setAttribute("class", "styleChart");
+            }                
     })       
 
-    // mouseout  pour enlever l'affichage le montant dépensé
+    // mouseout pour enlever l'affichage le montant dépensé
         chart.addEventListener("mouseout", () => {
             cleanPreviousDisplay(charts);
+          chart.classList.remove("styleChart");
         });
-
 })
 
-
+// Fonction pour désactiver tous les états précédents
 function cleanPreviousDisplay (els) {
     els.forEach(el => {
         if(el.children.length === 3)
@@ -46,6 +52,26 @@ function cleanPreviousDisplay (els) {
     })  
     charts[currentIndex].insertAdjacentHTML("beforeend", `<p> $${datas[currentIndex].amount} </p>`);
 }
+
+// Conversion obj en array of expenses
+datas.forEach(data => arrayExpense.push(data.amount));
+
+// Calcul dépense total semaine
+totalExpense = arrayExpense.reduce((prev, curr) => {
+    return prev + curr
+},0)
+
+// mise a jour du DOM total dépense
+totalExpenseDOM.textContent = "$"+ totalExpense.toFixed(2);
+
+// mise a jour DOM balance
+balanceDOM.textContent = "$"+ (currentBalance - totalExpense).toFixed(2)
+
+
+
+
+
+
 
 
 
